266402b61bb68dda0fbf4b63752e0f40886f2d75
Miquel Marin <mmarin_79@hotmail.com>
2021-02-15 14:37:34 +0100

    add onChangeAmount and onChangeCurrency

--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -4,10 +4,18 @@ export default function CurrencyRow({
   currencyOptions,
   selectedCurrency,
   onChangeCurrency,
+  onChangeAmount,
+  amount,
 }) {
+  console.log("amount", amount);
   return (
     <div>
-      <input className="input" type="number"></input>
+      <input
+        className="input"
+        type="number"
+        value={amount}
+        onChange={onChangeAmount}
+      ></input>
       <select value={selectedCurrency} onChange={onChangeCurrency}>
         {currencyOptions.map((option) => {
           return (
